f98f7a7ac3c258857bb0e83c3ea4e8fa3c5df7bc
Alexey Suprun <oleksiisu@wizardsdev.com>
2021-10-29 17:10:58 +0300

    [0.7] hotfix #2: fixes php warning on frontend which fired when there`s no e410 rule was active

--- admin/options/sections/custom-410s.php
+++ admin/options/sections/custom-410s.php
@@ -3,19 +3,21 @@
 function wdss_custom_410() {
   $requestUri = 'https://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
   $requestUri = urldecode($requestUri);
-  $e410s_rules_list = get_option('wdss_410s_dictionary');
+  $e410s_rules_list = get_option('wdss_410s_dictionary') ? get_option('wdss_410s_dictionary') : [];
+	
+  if(count($e410s_rules_list) > 0) {
+	  foreach($e410s_rules_list as $rule) {
+      $url = 'https://'.$_SERVER['HTTP_HOST'].$rule;
 
-  foreach($e410s_rules_list as $rule) {
-    $url = 'https://'.$_SERVER['HTTP_HOST'].$rule;
-
-    if($requestUri ==  $url) {
-      global $post, $wp_query;
-      $wp_query->set_404();
-      status_header(404);
-      header("HTTP/1.0 410 Gone");
-      include(get_query_template('404'));
-      exit;   
-    }
-  }
+      if($requestUri ==  $url) {
+        global $post, $wp_query;
+        $wp_query->set_404();
+        status_header(404);
+        header("HTTP/1.0 410 Gone");
+        include(get_query_template('404'));
+        exit;   
+      }
+	  }
+  }	
 }
 add_action( 'wp', 'wdss_custom_410' );
